ae753c743509c488dcb0c964c10989bfe1d74460
JoeCool12 <87907023+JoeCool12@users.noreply.github.com>
2021-07-27 22:10:25 -0400

    Add files via upload

---
+++ Track Phone Numbers/test.py
@@ -0,0 +1,7 @@
+'''Point of this file is to put in the
+phone number you want to track and make 
+sure to include the country code in the front"
+'''
+
+#For example if you live in America its +1
+number = "+1" #change this
