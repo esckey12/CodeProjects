accf03c003414946d5d0e4a5056e669792cd25bb
JoeCool12 <87907023+JoeCool12@users.noreply.github.com>
2021-09-15 13:46:39 -0400

    Update guesser.py

--- Python Projects/Guessing-game/guesser.py
+++ Python Projects/Guessing-game/guesser.py
@@ -1,1 +1,10 @@
+#Literally just try to guess the answer until you win. Fun game I know, 10/10. 
 
+from random import randrange
+n = randrange(1000)
+while True:
+   v = int(input())
+   if n == v:
+      print('You win!')
+      break
+   print('Smaller if (n < v) else 'Bigger')
